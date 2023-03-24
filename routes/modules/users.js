558160b53acefef8232ebd7da0bb362f6b18a18c
Chiang <tien23@gmail.com>
2023-03-24 12:32:36 +0800

    feat: add user register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,4 +8,11 @@ router.post('/login', (req, res) => {
   res.render('login')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.render('register')
+})
 module.exports = router
