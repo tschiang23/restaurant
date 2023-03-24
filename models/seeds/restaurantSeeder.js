b53a985eabe11c59d9ec4b34f3b9363b2e14fd1f
Chiang <tien23@gmail.com>
2023-03-25 03:56:26 +0800

    refactor: modify restaurantSeeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
-const restaurantList = require('./restaurant.json')
+const { users, restaurants } = require('./data')
 const db = require('../../config/mongoose')
+const User = require('../user')
+const bcrypt = require('bcrypt')
 
-db.once('open', () => {
-  Restaurant.create(restaurantList.results)
-    .then(() => {
-      console.log('done')
+db.once('open', async () => {
+  await Promise.all(
+    users.map(async (user, user_index) => {
+      const hash = await bcrypt.hash(user.password, 10)
+      user.password = hash
+      const createdUser = await User.create({ ...user })
+      console.log('user created')
+
+      const userRestaurant = []
+      restaurants.forEach((restaurant, rest_index) => {
+        if (rest_index >= 3 * user_index && rest_index < 3 * (user_index + 1)) {
+          restaurant.userId = createdUser._id
+          userRestaurant.push(restaurant)
+        }
+      })
+      await Restaurant.create(userRestaurant)
+      console.log('restaurant created')
     })
-    .catch((err) => {
-      console.log(err)
-    })
+  )
+
+  console.log('done')
+  process.exit()
 })
